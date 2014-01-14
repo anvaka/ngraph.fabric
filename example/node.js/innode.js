3810d0824dd59bc3ba1091a5295fd720db857853
anvaka <anvaka@gmail.com>
2014-01-14 04:08:58 -0800

    Updated node example

--- example/node.js/innode.js
+++ example/node.js/innode.js
@@ -1,28 +1,33 @@
 // This simple file will render graph into an image
   var graph = require('ngraph.generators').grid(40, 40);
-  var createFabric = require('../');
-  var size = 1280;
-  var fabricGraphics = createFabric(graph, {
-    width: size,
-    height: size
-  });
-  var layout = fabricGraphics.layout;
 
-  console.log('animating...');
-  for (var i = 0; i < 300; ++i) {
+  // we are going to use our own layout:
+  var layout = require('ngraph.forcelayout')(graph);
+  console.log('Running layout...');
+  for (var i = 0; i < 500; ++i) {
     layout.step();
   }
-  console.log('done');
-  var canvas = fabricGraphics.canvas;
+  console.log('Done. Rendering graph...');
 
+  var createFabric = require('../../');
+  var graphRect = layout.getGraphRect();
+  var size = Math.max(graphRect.x2 - graphRect.x1, graphRect.y2 - graphRect.y1) + 100;
+  var fabricGraphics = createFabric(graph, {
+    width: size,
+    height: size,
+    layout: layout
+  });
 
-  var scale = 0.3;
+  var scale = 1;
 
   fabricGraphics.setTransform(size/2, size/2, scale);
   fabricGraphics.renderOneFrame();
 
+  console.log('Done. Saving to file');
+
   var fs = require('fs');
   var out = fs.createWriteStream(__dirname + '/helloworld.png');
+  var canvas = fabricGraphics.canvas;
   var stream = canvas.createPNGStream();
   stream.on('data', function(chunk) {
     out.write(chunk);
